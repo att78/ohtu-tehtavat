ad355792f7203d530b3dba0afe268b30e73ff63c
Auli Tallberg <auli.tallberg@gmail.com>
2020-03-05 11:02:03 +0200

    nhl eka

--- viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
+++ viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
@@ -8,10 +8,11 @@ import java.util.List;
 public class Statistics {
 
     private List<Player> players;
+    private Reader r;
 
-    public Statistics() {
-        PlayerReader reader = new PlayerReader("https://nhlstatisticsforohtu.herokuapp.com/players.txt");
-        players = reader.getPlayers();       
+    public Statistics(Reader r) {
+        this.r = r;
+        players = r.getPlayers();       
     }
 
     public Player search(String name) {
