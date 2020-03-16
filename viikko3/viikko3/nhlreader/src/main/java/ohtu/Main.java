c814541459541034705dde46cc5a75e35f638752
Auli Tallberg <auli.tallberg@gmail.com>
2020-03-16 18:48:11 +0200

    vko3 eka tehty

--- viikko3/viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -5,10 +5,33 @@
  */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+                
+        System.out.println("json-muotoinen data:");
+        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        //sorttaus
+        Arrays.sort(players, (p1,p2)->p2.getGoals()+p2.getAssists()-p1.getGoals()-p1.getAssists());
+        
+        
+        System.out.println("Oliot:");
+        for (Player player : players) {
+            System.out.println(player);
+        }   
+    }
+  
 }
--- viikko3/viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,7 +3,13 @@ package ohtu;
 
 public class Player {
     private String name;
-
+    private Integer goals;
+    private Integer assists;
+    private String penalties;
+    private String team;
+    private String nationality;
+    private String birthdate;
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -14,7 +20,55 @@ public class Player {
 
     @Override
     public String toString() {
-        return name;
+        return name    +" "+goals+" "+assists+" "+penalties+" "+" "+team +" "+ " "+nationality+" "+birthdate;
+    }
+
+    public Integer getGoals() {
+        return goals;
+    }
+
+    public void setGoals(Integer goals) {
+        this.goals = goals;
+    }
+
+    public Integer getAssists() {
+        return assists;
+    }
+
+    public void setAssists(Integer assists) {
+        this.assists = assists;
+    }
+
+    public String getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(String penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
     }
       
 }
